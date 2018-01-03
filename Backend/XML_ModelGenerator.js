d7f0c5336eb555a412ea311b5c20b080d0be0ccb
Nehaw149 <nehaw149@gmail.com>
2018-01-03 12:20:56 +0100

    Committed minor errrors: _text,OverwrittenElements

--- Backend/XML_ModelGenerator.js
+++ Backend/XML_ModelGenerator.js
@@ -1,119 +1,145 @@
 var fs = require('fs');
-var XAML_json, XAML_ob, template_XAML_json, template_XAML_ob, element_json, model_json;
-var winOb, dockOb, stackOb, buttonOb, labelOb, radioOb;
-var sequence = 0;
+var XAML_json = '', XAML_ob = {}, template_XAML_json = '', templateXAMLob = {}, model_json = {};
+var winOb;
+var sequence = 0, win_flag = 0;
+var windowAttObject
 
 fs.readFile('./Parsed_XAML2.json', 'utf-8', function (err, data) {
   if (err)
     console.log(err);
   XAML_json = data.toString();
   XAML_ob = JSON.parse(XAML_json);
+  formatXAML_window(XAML_ob);
+})
 
+function formatXAML_window(XAMLob) {
   fs.readFile('./TEMPLATE_XAML_tags.json', 'utf-8', function (err, data) {
     if (err)
       console.log(err);
     template_XAML_json = data.toString();
-    template_XAML_ob = JSON.parse(template_XAML_json);
-    formatXAML_window(XAML_ob, template_XAML_ob);
-  })
-})
+    templateXAMLob = JSON.parse(template_XAML_json);
 
-function formatXAML_window(XAMLob, templateXAMLob) {
-  XAML_Object: for (var keys_XAML in XAMLob) {
-    elements:  for (var key_template in templateXAMLob.xaml.Elements) {
-      if (keys_XAML === key_template) {
-        sequence++;
-        var flag = 0
-        var windowAttObject = XAMLob[keys_XAML];
-        for (var key_win in windowAttObject) {
-          flag++
-          var childLen = key_win.length
-          //  console.log(childLen);
-          var childLen_Count = childLen
-          var win_atts_attsObj = windowAttObject[key_win];
-          if (key_win === "_attributes") {
-            childLen_Count--
-            winOb = { [keys_XAML]: { "_attributes": templateXAMLob.xaml.Elements[keys_XAML] } }
-            winOb[keys_XAML]._attributes.seq = sequence
-            
-            for (var win_attr in win_atts_attsObj) {
-              idm:  for (var key_winOb_idm in templateXAMLob.xaml.idm) {
-                //idm
-                if (win_attr === key_winOb_idm) {
-                  var winOb_idm = win_atts_attsObj[win_attr];
-                  winOb[keys_XAML]._attributes._text[win_attr] = winOb_idm
-                }else{
-                  break idm;
-                }
-                
-              }
-              properties: for (var key_winOb_prop in templateXAMLob.xaml.Elements[keys_XAML].properties) {
-                //properties
-                if (win_attr === key_winOb_prop) {
-                  var winOb_prop = win_atts_attsObj[win_attr];
-                  winOb[keys_XAML]._attributes.properties[win_attr] = winOb_prop
-                }
-              }
-              events: for (var key_winOb_events in templateXAMLob.xaml.Elements[keys_XAML].events) {
-                //events
-                if (win_attr === key_winOb_events) {
-                  var winOb_event = win_atts_attsObj[win_attr];
-                  winOb[keys_XAML]._attributes.events[win_attr] = winOb_event
-                }
-              }
-              com_attr: for (var key_winOb_com_attr in templateXAMLob.xaml.com_attr) {
-                if (win_attr === key_winOb_com_attr) {
-                  var winOb_com_attr = win_atts_attsObj[win_attr];
-                  winOb[keys_XAML]._attributes.properties[win_attr] = winOb_com_attr
-                }
-                
-              }
-              com_events: for (var key_winOb_com_events in templateXAMLob.xaml.com_events) {
-                if (win_attr === key_winOb_com_events) {
-                  var winOb_com_events = win_atts_attsObj[win_attr];
-                  winOb[keys_XAML]._attributes.events[win_attr] = winOb_com_events
-                }
-                
-              }
+    var windowAttObject = {}, win_atts_Obj = {}, key_win = '', keys_XAML, key_template = '';
+    var win_attr = '', key_winOb_idm = '', key_winOb_prop = '', key_winOb_events = '', key_winOb_com_attr = '', key_winOb_com_events = '', key_winOb_text = '';
+    var winOb_idm = {}, winOb_prop = {}, winOb_com_events = {}, winOb_com_attr = {}, winOb_event = {};
+    for (key_win in windowAttObject) { delete windowAttObject[key_win]; key_win = '' }
+    for (key_win in winOb) { delete winOb[key_win]; key_win = '' }
+    for (key_win in win_atts_Obj) { delete win_atts_Obj[key_win]; key_win = '' }
+    var temp_com_attr_Obj = {}, temp_com_events_Obj = {}, temp_props_Obj = {}, temp_events_Obj = {}, temp_idm_Obj = {}, temp_ele_Obj = {}, temp_text_Obj = {};
+
+    temp_com_attr_Obj = templateXAMLob.xaml.com_attr;
+    temp_com_events_Obj = templateXAMLob.xaml.com_events;
+    temp_idm_Obj = templateXAMLob.xaml.idm;
+    temp_ele_Obj = templateXAMLob.xaml.Elements;
+
+    XAML_Object: for (keys_XAML in XAMLob) {
+      elements: for (key_template in temp_ele_Obj) {
+        if (keys_XAML === key_template) {
+          sequence++;
+          temp_props_Obj = templateXAMLob.xaml.Elements[keys_XAML].properties;
+          temp_events_Obj = templateXAMLob.xaml.Elements[keys_XAML].events;
+          temp_text_Obj = templateXAMLob.xaml.Elements[keys_XAML];
+          windowAttObject = XAMLob[keys_XAML];
+
+          arrayAttr: for (var len = 0; len < windowAttObject.length; len++) {
+            if (len >= 0) {
+              setAttributes(windowAttObject[len])
+              sequence++
             }
-            for (var key_clearVal1 in winOb) {
-              for (var key_clearVal2 in winOb[key_clearVal1]) {
-                for (var key_clearVal3 in winOb[key_clearVal1][key_clearVal2]) {
-                  for (var key_clearVal4 in winOb[key_clearVal1][key_clearVal2][key_clearVal3]) {
-                    var ob_attr1 = winOb[key_clearVal1][key_clearVal2][key_clearVal3]
-                    if (ob_attr1[key_clearVal4] === "") {
-                      delete ob_attr1[key_clearVal4]
+            else {
+              break arrayAttr;
+            }
+          }
+          setAttributes(windowAttObject)
+          function setAttributes(windowAttObject) {
+            // if(winOb === {} && win_flag === 0){
+            //   winOb = {xaml: [sequence] }
+            //   win_flag++
+            // }else{
+            //   winOb = {xaml: [winOb]}
+            // }
+            // console.log(JSON.stringify(winOb))
+            for (key_win in windowAttObject) {
+              win_atts_Obj = windowAttObject[key_win];
+              if (key_win === "_attributes") {
+                winOb = {[sequence]: [keys_XAML]}
+                winOb[sequence] = { [keys_XAML]: { "_attributes": templateXAMLob.xaml.Elements[keys_XAML] } }
+                winOb[sequence][keys_XAML]._attributes.seq = sequence
+
+                for (win_attr in win_atts_Obj) {
+                  key_winOb_idm = ''
+                  idm: for (key_winOb_idm in temp_idm_Obj) {
+                    //idm
+                    if (win_attr === key_winOb_idm) {
+                      winOb[sequence][keys_XAML]._attributes._text[win_attr] = win_atts_Obj[win_attr];
+                      break idm
+                    }
+                  }
+                  key_winOb_com_attr = ''
+                  com_attr: for (key_winOb_com_attr in temp_com_attr_Obj) {
+                    if (win_attr === key_winOb_com_attr) {
+                      winOb[sequence][keys_XAML]._attributes.properties[win_attr] = win_atts_Obj[win_attr];
+                    }
+                  }
+                  key_winOb_com_events = ''
+                  com_events: for (key_winOb_com_events in temp_com_events_Obj) {
+                    //com_events
+                    if (win_attr === key_winOb_com_events) {
+                      winOb[sequence][keys_XAML]._attributes.events[win_attr] = win_atts_Obj[win_attr];
+                    }
+                  }
+                  key_winOb_prop = ''
+                  properties: for (key_winOb_prop in temp_props_Obj) {
+                    if (win_attr === key_winOb_prop) {
+                      winOb[sequence][keys_XAML]._attributes.properties[win_attr] = win_atts_Obj[win_attr];
+                    }
+                  }
+                  key_winOb_events = ''
+                  events: for (key_winOb_events in temp_events_Obj) {
+                    //events
+                    if (win_attr === key_winOb_events) {
+                      winOb[sequence][keys_XAML]._attributes.events[win_attr] = win_atts_Obj[win_attr];
                     }
                   }
                 }
+                for (var key_clearVal1 in winOb) {
+                  for (var key_clearVal2 in winOb[key_clearVal1]) {
+                    for (var key_clearVal3 in winOb[key_clearVal1][key_clearVal2]) {
+                      for (var key_clearVal4 in winOb[key_clearVal1][key_clearVal2][key_clearVal3]) {
+                        for (var key_clearVal5 in winOb[key_clearVal1][key_clearVal2][key_clearVal3][key_clearVal4]) {
+                          var ob_attr1 = winOb[key_clearVal1][key_clearVal2][key_clearVal3][key_clearVal4]
+                          if (ob_attr1[key_clearVal5] === "") {
+                            delete ob_attr1[key_clearVal5]
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+                console.log(JSON.stringify(winOb))
               }
-            }
-            console.log(JSON.stringify(winOb))
-          }
-          if(key_win === "_text"){
-            var winOb_text = win_atts_attsObj[key_win];
-
-          //  console.log(JSON.stringify(winOb_text))
-
-            for (var key_winOb_text in templateXAMLob.xaml.Elements[keys_XAML]._text) {
-              if (key_win === key_winOb_text) {
-              //  var winOb_text = win_atts_attsObj[key_win];
-                winOb[keys_XAML]._attributes._text[key_win] = winOb_text
+              key_winOb_text = ''
+              if (key_win === "_text") {
+                _text: for (key_winOb_text in temp_text_Obj) {
+                  if (key_win === key_winOb_text) {
+                    winOb[sequence][keys_XAML]._attributes._text = windowAttObject[key_win];
+                  }
+                }
               }
+              if (key_win != "_attributes" && key_win != "_text") {
+                var parsedEleOb = windowAttObject                
+                formatXAML_window(parsedEleOb)
+                break;
+              }
+              //  setModel(winOb)
+            //  console.log(JSON.stringify(winOb))              
             }
           }
-          if (key_win != "_attributes" && key_win!= "_text" && childLen_Count > 0) {
-            childLen_Count--
-            var parsedEleOb = windowAttObject
-            //  console.log(JSON.stringify(parsedEleOb) + key_win)
-            formatXAML_window(parsedEleOb, templateXAMLob)
-          }
-
         }
       }
-    }    
-  }
-  //  console.log(JSON.stringify(winOb));
+    }
+    //  console.log(JSON.stringify(winOb));
+  })
 }
 
 function error_outScoped() {
