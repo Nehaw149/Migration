6ffdc55ae25ba8ce617aa448b1e4dcce3fcdc099
Nehaw149 <nehaw149@gmail.com>
2018-01-31 15:21:16 +0100

    Add files via upload

---
+++ Backend/OUTPUT/FUI/HTML/FUI_JS.js
@@ -0,0 +1,36 @@
+{
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById('id_1').setAttribute('title', 'MainWindow');
+	document.getElementById('id_11').setAttribute('innerHTML', 'Hover');
+	document.getElementById('id_11').setAttribute('disabled', 'false');
+	document.getElementById('id_11').setAttribute('name', 'button1');
+	document.getElementById('id_10').setAttribute('checked', 'false');
+	document.getElementById('id_10').setAttribute('name', 'Age');
+	document.getElementById('id_9').setAttribute('checked', 'false');
+	document.getElementById('id_9').setAttribute('innerHTML', 'Female');
+	document.getElementById('id_9').setAttribute('name', 'female');
+	document.getElementById('id_8').setAttribute('checked', 'false');
+	document.getElementById('id_8').setAttribute('innerHTML', 'Male');
+	document.getElementById('id_8').setAttribute('name', 'male');
+	document.getElementById('id_7').setAttribute('innerHTML', 'Gender:');
+	document.getElementById('id_6').setAttribute('maxlength', '8');
+	document.getElementById('id_6').setAttribute('name', 'pwBox');
+	document.getElementById('id_5').setAttribute('innerHTML', 'Password');});
+
+
+if(document.getElementById('id_11')){
+	document.getElementById('id_11').addEventListener('onclick', OnClick1)}; 
+function OnClick1(){}
+if(document.getElementById('id_10')){
+	document.getElementById('id_10').addEventListener('onclick', HandleCheck)}; 
+function HandleCheck(){}
+if(document.getElementById('id_10')){
+	document.getElementById('id_10').addEventListener('onclick', HandleUnchecked)}; 
+function HandleUnchecked(){}
+if(document.getElementById('id_9')){
+	document.getElementById('id_9').addEventListener('onclick', HandleCheck)}; 
+function HandleCheck(){}
+if(document.getElementById('id_8')){
+	document.getElementById('id_8').addEventListener('onclick', HandleCheck)}; 
+function HandleCheck(){}
+}
